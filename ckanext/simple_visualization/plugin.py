51d8a05805d58d12f5bad90f72b72d7fde0be8b9
Sayan <Bianca.Sayan@ontario.ca>
2018-09-07 16:04:05 -0400

    feature: basic plugin with simple smarties view that shows 3 manually-entered values and 3 manually entered labels

--- ckanext/simple_visualization/plugin.py
+++ ckanext/simple_visualization/plugin.py
@@ -1,13 +1,65 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import logging
 
+not_empty = plugins.toolkit.get_validator('not_empty')
 
 class Simple_VisualizationPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-
-    # IConfigurer
+    plugins.implements(plugins.IConfigurer, inherit=True)
+    plugins.implements(plugins.IResourceView, inherit=True)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'simple_visualization')
+    '''
+    This extension views a hard-coded visual summary of a resource.
+    '''
+    def info(self):
+        return {'name': 'simple_visualization',
+                'title': 'Smarties Summary',
+                'icon': 'table',
+                'requires_datastore': False,
+                'always_available': True,
+                'schema': {
+                    'smartie_value_1': [not_empty],
+                    'smartie_value_2': [not_empty],
+                    'smartie_value_3': [not_empty],
+                    'smartie_label_1': [not_empty],
+                    'smartie_label_2': [not_empty],
+                    'smartie_label_3': [not_empty]
+                },
+                'default_title': plugins.toolkit._('Smarties Summary'),
+                }
+
+    def can_view(self, data_dict):
+      return True
+
+    def view_template(self, context, data_dict):
+        return 'smarties_view.html'
+
+    def form_template(self, context, data_dict):
+        return 'smarties_form.html'
+
+    '''
+class SmartiesView(Simple_VisualizationPlugin):
+
+    def info(self):
+        return {'name': 'simple_visualization',
+                'title': 'Smarties Summary',
+                'icon': 'table',
+                'requires_datastore': False,
+                'always_available': True,
+                'default_title': plugins.toolkit._('Smarties Summary'),
+                }
+
+    def can_view(self, data_dict):
+      return True
+
+    def view_template(self, context, data_dict):
+        return 'smarties_view.html'
+
+    def form_template(self, context, data_dict):
+        return 'smarties_form.html'
+
+    '''
